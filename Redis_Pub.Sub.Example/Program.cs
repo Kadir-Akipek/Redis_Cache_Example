dad36426d15b4bd8d943fe69c3d668f19a72d7af
Kadir Akipek <akipekkadir23@gmail.com>
2025-06-07 22:22:09 +0300

    pub/sub is created

---
+++ Redis_Pub.Sub.Example/Program.cs
@@ -0,0 +1,15 @@
+﻿using StackExchange.Redis;
+
+ConnectionMultiplexer connection =  await ConnectionMultiplexer.ConnectAsync("localhost:1453");
+
+ISubscriber subscriber =  connection.GetSubscriber(); //subscriber oluşturduk
+
+while (true)
+{
+	Console.Write("Mesaj : ");
+	string message = Console.ReadLine();
+	await subscriber.PublishAsync("mychannel", message);
+}
+
+
+
