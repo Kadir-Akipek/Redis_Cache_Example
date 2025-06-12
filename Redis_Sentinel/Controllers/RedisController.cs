744ca421b0dbf3ac01232844673ba288bca1da69
Kadir Akipek <akipekkadir23@gmail.com>
2025-06-12 15:03:23 +0300

    sentinel structure added

---
+++ Redis_Sentinel/Controllers/RedisController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Redis_Sentinel.Services;
+
+namespace Redis_Sentinel.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class RedisController : ControllerBase
+	{
+		//localhost:4200/api/redis/setvalue/name/gencay
+		[HttpGet("[action]/{key}/{value}")]
+		public async Task<IActionResult> SetValue(string key, string value)
+		{
+			var redis = await RedisService.RedisMasterDatabase();
+			await redis.StringSetAsync(key, value);	
+			return Ok(value);
+		}
+
+		//localhost:4200/api/redis/getvalue/name
+		[HttpGet("[action]/{key}")]
+		public async Task<IActionResult> GetValue(string key)
+		{
+			var redis = await RedisService.RedisMasterDatabase();
+			var data = await redis.StringGetAsync(key);
+			return Ok(data.ToString());
+		}
+	}
+}
